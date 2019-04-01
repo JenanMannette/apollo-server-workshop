316f441ebef31d0c83a5d9c54984a1a70c287649
Sean Connolly <sconnolly17@gmail.com>
2019-04-01 15:43:48 -0600

    Exercise 2 solution

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const server = express();
 
@@ -6,6 +7,21 @@ const server = express();
 server.get('/health', (req, res) => {
   res.send('ok');
 });
+
+// Exercise2 Solution
+const apolloServer = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      anyNumber: String
+    }
+  `,
+  resolvers: {
+    Query: {
+      anyNumber: () => 'Threeve',
+    },
+  },
+});
+apolloServer.applyMiddleware({ app: server });
 
 // eslint-disable-next-line no-console
 server.listen(3000, () => console.log('listening on port 3000'));
